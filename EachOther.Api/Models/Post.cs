8d99f3f4f585544607e47b1553a109eff00cb1de
lixinyang123 <lixinyangemil@outlook.com>
2020-08-18 18:24:12 +0800

    完善代码

--- EachOther.Api/Models/Post.cs
+++ EachOther.Api/Models/Post.cs
@@ -6,6 +6,12 @@ namespace EachOther.Api.Models
     {
         public string Id {get; set;}
 
+        public string Title {get; set;}
+
+        public string Overview {get; set;}
+
+        public string Category {get; set;}
+
         public DateTime Date {get; set;}
     }
 }
