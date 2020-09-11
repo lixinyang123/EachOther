94da3fb55e8c97ca4393bbcb30ad701e33f0c9e2
lixinyang123 <lixinyangemil@outlook.com>
2020-09-11 15:44:22 +0800

    动态展示

--- wwwroot/js/article/index.js
+++ wwwroot/js/article/index.js
@@ -1,13 +1,16 @@
 function getArticles(index) {
 
-    var url = "/Article/GetArticles?index=" + index;
+    let url = "/Article/GetArticles?index=" + index;
 
     $.ajax({
         url: url,
         type: 'get',
         dataType: 'text',
         success: function (data) {
-            console.log(JSON.parse(data.toString()));
+            let articles = JSON.parse(data.toString());
+            articles.forEach(element => {
+                createArticle(element);
+            });
         },
         error: error => console.log(error)
     });
@@ -16,21 +19,22 @@ function getArticles(index) {
 function createArticle(article) {
     var html = `
         <div class="col-md-4 d-flex">
-            <div class="blog-entry active">
-                <a asp-controller="Article" asp-action="Detail" class="img" style="background-image: url(/images/f.jpg);"></a>
+            <div class="blog-entry">
+                <a asp-controller="Article" asp-action="Detail" class="img" style="background-image: url(${article.CoverUrl});"></a>
                 <div class="text p-4">
-                    <h3 class="mb-2"><a asp-controller="Article" asp-action="Detail">宝贝，七夕快乐~</a></h3>
+                    <h3 class="mb-2"><a asp-controller="Article" asp-action="Detail">${article.Title}</a></h3>
                     <div class="meta-wrap">
                         <p class="meta">
-                            <span>Aug. 24, 2020</span>
+                            <span>${article.Date}</span>
                             <span>0 Comment</span>
                         </p>
                     </div>
-                    <p class="mb-4">宝贝，我欠你的500字情书</p>
-                    <p class="mb-4">希望你能喜欢😘</p>
+                    <p class="mb-4">${article.Overview}</p>
                     <p><a asp-controller="Article" asp-action="Detail" class="btn-custom">Read More</span></a></p>
                 </div>
             </div>
         </div>
     `;
+
+    document.querySelector("#post > div").innerHTML += html;
 }
