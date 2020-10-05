5a071243e5997a7bbdffd4b3befa307658bf9a07
lixinyang123 <lixinyangemil@outlook.com>
2020-10-06 03:36:32 +0800

    完善管理页面分页

--- Controllers/ManagerController.cs
+++ Controllers/ManagerController.cs
@@ -28,9 +28,25 @@ namespace EachOther.Controllers
             this.notifyService = notifyService;
             pageSize = configuration.GetValue<int>("PageSize");
         }
+
+         //矫正页码
+        private int CorrectIndex(int index, int pageCount)
+        {
+            //页码<1时留在第一页
+            index = index < 1 ? 1 : index;
+            //页码>总页数时留在最后一页
+            index = index > pageCount ? pageCount : index;
+            //如果没有博客时留在第一页
+            index = pageCount == 0 ? 1 : index;
+            return index;
+        }
         
         public IActionResult Index(int index = 1)
         {
+            int pageCount = Convert.ToInt32(Math.Ceiling(Convert.ToDouble(articleDbContext.Articles.Count()) / pageSize));
+            index = CorrectIndex(index,pageCount);
+            ViewBag.CurrentIndex = index;
+            
             string user = Request.Cookies["user"];
             List<Article> articles = articleDbContext.Articles
                 .OrderByDescending(i=>i.Id)
