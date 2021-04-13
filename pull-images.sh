aa5dd1c543863802b4b1cd0719fcdd3cb58a5981
LLLXY <lixinyangemil@outlook.com>
2021-04-13 15:06:19 +0800

    Update pull-images.sh

--- pull-images.sh
+++ pull-images.sh
@@ -1,4 +1,5 @@
 docker pull registry.cn-shenzhen.aliyuncs.com/lllxy/eachother:latest
 docker tag registry.cn-shenzhen.aliyuncs.com/lllxy/eachother:latest lixinyang/eachother:latest
 docker rmi registry.cn-shenzhen.aliyuncs.com/lllxy/eachother:latest
+docker pull mysql
 docker images  | grep none | awk '{print $3}' | xargs docker rmi
