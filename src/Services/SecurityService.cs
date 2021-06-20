853950c8367bf0fad233ec5df254b9d3d0db8622
lixinyang123 <lixinyangemil@outlook.com>
2021-06-20 14:42:41 +0800

    更新旧api

--- src/Services/SecurityService.cs
+++ src/Services/SecurityService.cs
@@ -7,17 +7,15 @@ namespace EachOther.Services
 {
     public class SecurityService : StaticConfig<Secret>
     {
-        private readonly RijndaelManaged rijndaelManaged;
+        private readonly Aes aes;
 
         public SecurityService(string fileName, Secret initSecret) : base(fileName, initSecret)
         {
-            rijndaelManaged = new RijndaelManaged
-            {
-                IV = Encoding.UTF8.GetBytes(Config.IV),
-                Key = Encoding.UTF8.GetBytes(Config.Key),
-                Mode = CipherMode.ECB,
-                Padding = PaddingMode.PKCS7
-            };
+            aes = Aes.Create();
+            aes.IV = Encoding.UTF8.GetBytes(Config.IV);
+            aes.Key = Encoding.UTF8.GetBytes(Config.Key);
+            aes.Mode = CipherMode.ECB;
+            aes.Padding = PaddingMode.PKCS7;
         }
 
         public string Encrypt(string str)
@@ -25,7 +23,7 @@ namespace EachOther.Services
             if (string.IsNullOrEmpty(str)) return null;
             byte[] toEncryptArray = Encoding.UTF8.GetBytes(str);
 
-            ICryptoTransform cTransform = rijndaelManaged.CreateEncryptor();
+            ICryptoTransform cTransform = aes.CreateEncryptor();
             byte[] resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
 
             return Convert.ToBase64String(resultArray, 0, resultArray.Length);
@@ -36,7 +34,7 @@ namespace EachOther.Services
             if (string.IsNullOrEmpty(str)) return null;
             byte[] toEncryptArray = Convert.FromBase64String(str);
 
-            ICryptoTransform cTransform = rijndaelManaged.CreateDecryptor();
+            ICryptoTransform cTransform = aes.CreateDecryptor();
             byte[] resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
 
             return Encoding.UTF8.GetString(resultArray);
